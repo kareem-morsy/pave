bb2c1806fb6780b831cce8406c39ca24573a8359
karem-morsy <kareem.morsy2014@gmail.com>
2022-12-01 16:21:28 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,104 @@
+//scroll counter
+document.addEventListener("DOMContentLoaded", function () {
+    // You can change this class to specify which elements are going to behave as counters.
+    var elements = document.querySelectorAll(".scroll-counter")
+
+    elements.forEach(function (item) {
+        // Add new attributes to the elements with the '.scroll-counter' HTML class
+        item.counterAlreadyFired = false
+        item.counterSpeed = item.getAttribute("data-counter-time") / 45
+        item.counterTarget = +item.innerText
+        item.counterCount = 0
+        item.counterStep = item.counterTarget / item.counterSpeed
+
+        item.updateCounter = function () {
+            item.counterCount = item.counterCount + item.counterStep
+            item.innerText = Math.ceil(item.counterCount)
+
+            if (item.counterCount < item.counterTarget) {
+                setTimeout(item.updateCounter, item.counterSpeed)
+            } else {
+                item.innerText = item.counterTarget
+            }
+        }
+    })
+
+    // Function to determine if an element is visible in the web page
+    var isElementVisible = function isElementVisible(el) {
+        var scroll = window.scrollY || window.pageYOffset
+        var boundsTop = el.getBoundingClientRect().top + scroll
+        var viewport = {
+            top: scroll,
+            bottom: scroll + window.innerHeight,
+        }
+        var bounds = {
+            top: boundsTop,
+            bottom: boundsTop + el.clientHeight,
+        }
+        return (
+            (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
+            (bounds.top <= viewport.bottom && bounds.top >= viewport.top)
+        )
+    }
+
+    // Funciton that will get fired uppon scrolling
+    var handleScroll = function handleScroll() {
+        elements.forEach(function (item, id) {
+            if (true === item.counterAlreadyFired) return
+            if (!isElementVisible(item)) return
+            item.updateCounter()
+            item.counterAlreadyFired = true
+        })
+    }
+
+    // Fire the function on scroll
+    window.addEventListener("scroll", handleScroll)
+})
+
+
+
+$('.partners-slider').slick({
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    // centerPadding: '45px',
+    speed: 200,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+
+    ]
+});
+
+
+$('.Testimonials-slider').slick();
+
+
+
+
+//mixitup 
+var containerEl = document.querySelector('.contain');
+var mixer = mixitup(containerEl);
+
